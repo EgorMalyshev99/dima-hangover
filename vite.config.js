b9c00d6d279e9f126d25cac345a252f2b79f640d
Egor Malyshev <malyshev.e@drim.one>
2025-08-22 15:41:28 +0300

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/dima/' : './',
+  base: process.env.NODE_ENV === "production" ? "/dima-hangover/" : "./",
   build: {
     outDir: "dist",
     assetsDir: "assets",
